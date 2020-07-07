e37941cfc28b18b373214613214e44b23de51652
xpetit <32063953+xpetit@users.noreply.github.com>
2020-07-07 13:59:19 +0200

    Use available instead of free

--- mem.go
+++ mem.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var total = readMem("MemTotal:")
+
 func readMem(parameter string) int {
 	lines := readLines("/proc/meminfo")
 	for _, line := range lines {
@@ -14,17 +16,16 @@ func readMem(parameter string) int {
 		if fields[0] == parameter {
 			i, err := strconv.Atoi(fields[1])
 			check(err)
-			return int(math.Round(float64(i*1024) / 1_000_000_000)) // Kio to Go
+			return int(math.Round(float64(i*1024) / 1_000_000_000)) // Kio to GB
 		}
 	}
 	panic(parameter + " not found in /proc/meminfo")
 }
 
-// mem returns "used+free GB"
+// mem returns "used+available GB"
 func mem() string {
-	total := readMem("MemTotal:")
-	free := readMem("MemFree:")
-	used := total - free
+	available := readMem("MemAvailable:") - 1 // minus 1 GB because Linux...
+	used := total - available
 	padding := 2*len(strconv.Itoa(total)) + 1
-	return fmt.Sprintf("%*s GB", padding, fmt.Sprintf("%d+%d", used, free))
+	return fmt.Sprintf("%*s GB", padding, fmt.Sprintf("%d+%d", used, available))
 }
